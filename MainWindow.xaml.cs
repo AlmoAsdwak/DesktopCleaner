384470b18dbf9a3493379469d7dff80908a3e621
Libor Šenar <liborsenar1@gmail.com>
2024-02-15 21:44:34 +0100

    Better checkbox managment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Runtime.Intrinsics.X86;
-using System.Text;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace DesktopCleaner
 {
-
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public string[] pictures = { "*.jpg", "*.jpeg", "*.gif", "*.psd", "*.tiff", "*.tif", "*.png", "*.bmp", "*.raw", "*.nef", "*.ai" };
@@ -31,256 +16,107 @@ namespace DesktopCleaner
         public string[] musics = { "*.mp3", "*.wav", "*.flac", "*.midi" };
         public string[] shortcuts = { "*.lnk" };
         public string[] printing = { "*.stl", "*.gcode", "*.obj", "*.3ds", "*.blend", "*.skp", "*.max", "*.3dmf", "*.3mf" };
-        public int pictureselection = 0;
-        public int archiveselection = 0;
-        public int documentselection = 0;
-        public int videoselection = 0;
-        public int torrentselection = 0;
-        public int executableselection = 0;
-        public int cinemaeffectselection = 0;
-        public int musicselection = 0;
-        public int shortcutselection = 0;
-        public int printingselection = 0;
+        public bool pictureselection = false;
+        public bool archiveselection = false;
+        public bool documentselection = false;
+        public bool videoselection = false;
+        public bool torrentselection = false;
+        public bool executableselection = false;
+        public bool cinemaeffectselection = false;
+        public bool musicselection = false;
+        public bool shortcutselection = false;
+        public bool printingselection = false;
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void PictureSelection(object sender, RoutedEventArgs e)
+        private void SelectionChanged(object sender, RoutedEventArgs e)
         {
-            if (pictureselection == 0)
-                pictureselection++;
-            else
-                pictureselection--;
-        }
+#pragma warning disable
+            CheckBox checkBox = sender as CheckBox;
+            string content = checkBox.Content.ToString();
+#pragma warning enable
 
-        private void ArchiveSelection(object sender, RoutedEventArgs e)
-        {
-            if (archiveselection == 0)
-                archiveselection++;
-            else
-                archiveselection--;
-        }
-
-        private void DocumentSelection(object sender, RoutedEventArgs e)
-        {
-            if (documentselection == 0)
-                documentselection++;
-            else
-                documentselection--;
-        }
-
-        private void VideoSelection(object sender, RoutedEventArgs e)
-        {
-            if (videoselection == 0)
-                videoselection++;
-            else
-                videoselection--;
-        }
-
-        private void TorrentSelection(object sender, RoutedEventArgs e)
-        {
-            if (torrentselection == 0)
-                torrentselection++;
-            else
-                torrentselection--;
-        }
-
-        private void ExecutableSelection(object sender, RoutedEventArgs e)
-        {
-            if (executableselection == 0)
-                executableselection++;
-            else
-                executableselection--;
-        }
-
-        private void CinemaEffectSelection(object sender, RoutedEventArgs e)
-        {
-            if (cinemaeffectselection == 0)
-                cinemaeffectselection++;
-            else
-                cinemaeffectselection--;
-        }
-
-        private void MusicSelection(object sender, RoutedEventArgs e)
-        {
-            if (musicselection == 0)
-                musicselection++;
-            else
-                musicselection--;
-        }
-
-        private void ShortcutSelection(object sender, RoutedEventArgs e)
-        {
-            if (shortcutselection == 0)
-                shortcutselection++;
-            else
-                shortcutselection--;
-        }
-        private void Printer(object sender, RoutedEventArgs e)
-        {
-            if (printingselection == 0)
-                printingselection++;
-            else
-                printingselection--;
+            switch (content)
+            {
+                case "Obrázky":
+                    pictureselection = !pictureselection;
+                    break;
+                case "Archívy":
+                    archiveselection = !archiveselection;
+                    break;
+                case "Dokumenty":
+                    documentselection = !documentselection;
+                    break;
+                case "Videa":
+                    videoselection = !videoselection;
+                    break;
+                case "Torrenty":
+                    torrentselection = !torrentselection;
+                    break;
+                case "Executable":
+                    executableselection = !executableselection;
+                    break;
+                case "Cinema Efekty":
+                    cinemaeffectselection = !cinemaeffectselection;
+                    break;
+                case "Hudba":
+                    musicselection = !musicselection;
+                    break;
+                case "Zástupce":
+                    shortcutselection = !shortcutselection;
+                    break;
+                case "3D Tisk":
+                    printingselection = !printingselection;
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void Clean(object sender, RoutedEventArgs e)
         {
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string RootPath = System.IO.Path.Combine(desktopPath, @"DesktopCleaner\");
+            string RootPath = Path.Combine(desktopPath, @"DesktopCleaner\");
+
             if (Directory.Exists(RootPath))
             {
                 VelkeText.Content = "Na ploše již existuje složka DesktopCleaner, prosím přejmenujte nebo přestuňte tuto složku";
                 return;
             }
+
             Directory.CreateDirectory(RootPath);
-            if (pictureselection == 1)
+
+            Dictionary<string, (bool selection, string[] items)> categories = new Dictionary<string, (bool, string[])>()
             {
-                string PicPath = System.IO.Path.Combine(RootPath, @"Fotky\");
-                Directory.CreateDirectory(PicPath);
-                foreach (var item in pictures)
-                {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var photo in tmp)
-                    {
-                        string a = photo.Substring(photo.LastIndexOf('\\')+1);
-                        string tmp2 = System.IO.Path.Combine(PicPath, a);
-                        File.Move(photo, tmp2);
-                    }
-                }
-            }
-            if (archiveselection == 1)
+                { "Fotky", (pictureselection, pictures) },
+                { "Archivy", (archiveselection, archives) },
+                { "Dokumenty", (documentselection, documents) },
+                { "Videa", (videoselection, videos) },
+                { "Torrenty", (torrentselection, torrents) },
+                { "Executables", (executableselection, executable) },
+                { "CinemaEfekty", (cinemaeffectselection, cinemaeffect) },
+                { "Hudba", (musicselection, musics) },
+                { "Zastupce", (shortcutselection, shortcuts) },
+                { "3DPrinting", (printingselection, printing) }
+            };
+
+            foreach (var category in categories)
             {
-                string ArchPath = System.IO.Path.Combine(RootPath, @"Archivy\");
-                Directory.CreateDirectory(ArchPath);
-                foreach (var item in archives)
+                if (!category.Value.selection)
+                    continue;
+
+                string categoryPath = Path.Combine(RootPath, category.Key);
+                Directory.CreateDirectory(categoryPath);
+
+                foreach (var item in category.Value.items)
                 {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var arch in tmp)
+                    string[] files = Directory.GetFiles(desktopPath, item);
+                    foreach (var file in files)
                     {
-                        string a = arch.Substring(arch.LastIndexOf('\\') + 1);
-                        string tmp2 = System.IO.Path.Combine(ArchPath, a);
-                        File.Move(arch, tmp2);
-                    }
-                }
-            }
-            if (documentselection == 1)
-            {
-                string DocPath = System.IO.Path.Combine(RootPath, @"Dokumenty\");
-                Directory.CreateDirectory(DocPath);
-                foreach (var item in documents)
-                {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var document in tmp)
-                    {
-                        string a = document.Substring(document.LastIndexOf('\\') + 1);
-                        string tmp2 = System.IO.Path.Combine(DocPath, a);
-                        File.Move(document, tmp2);
-                    }
-                }
-            }
-            if (videoselection == 1)
-            {
-                string VidPath = System.IO.Path.Combine(RootPath, @"Videa\");
-                Directory.CreateDirectory(VidPath);
-                foreach (var item in videos)
-                {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var video in tmp)
-                    {
-                        string a = video.Substring(video.LastIndexOf('\\') + 1);
-                        string tmp2 = System.IO.Path.Combine(VidPath, a);
-                        File.Move(video, tmp2);
-                    }
-                }
-            }
-            if (torrentselection == 1)
-            {
-                string TorPath = System.IO.Path.Combine(RootPath, @"Torrenty\");
-                Directory.CreateDirectory(TorPath);
-                foreach (var item in torrents)
-                {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var torrent in tmp)
-                    {
-                        string a = torrent.Substring(torrent.LastIndexOf('\\') + 1);
-                        string tmp2 = System.IO.Path.Combine(TorPath, a);
-                        File.Move(torrent, tmp2);
-                    }
-                }
-            }
-            if (executableselection == 1)
-            {
-                string ExePath = System.IO.Path.Combine(RootPath, @"Executables\");
-                Directory.CreateDirectory(ExePath);
-                foreach (var item in executable)
-                {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var executablez in tmp)
-                    {
-                        string a = executablez.Substring(executablez.LastIndexOf('\\') + 1);
-                        string tmp2 = System.IO.Path.Combine(ExePath, a);
-                        File.Move(executablez, tmp2);
-                    }
-                }
-            }
-            if (cinemaeffectselection == 1)
-            {
-                string CinPath = System.IO.Path.Combine(RootPath, @"CinemaEfekty\");
-                Directory.CreateDirectory(CinPath);
-                foreach (var item in cinemaeffect)
-                {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var effect in tmp)
-                    {
-                        string a = effect.Substring(effect.LastIndexOf('\\') + 1);
-                        string tmp2 = System.IO.Path.Combine(CinPath, a);
-                        File.Move(effect, tmp2);
-                    }
-                }
-            }
-            if (musicselection == 1)
-            {
-                string MusPath = System.IO.Path.Combine(RootPath, @"Hudba\");
-                Directory.CreateDirectory(MusPath);
-                foreach (var item in musics)
-                {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var music in tmp)
-                    {
-                        string a = music.Substring(music.LastIndexOf('\\') + 1);
-                        string tmp2 = System.IO.Path.Combine(MusPath, a);
-                        File.Move(music, tmp2);
-                    }
-                }
-            }
-            if (shortcutselection == 1)
-            {
-                string ShoPath = System.IO.Path.Combine(RootPath, @"Zastupce\");
-                Directory.CreateDirectory(ShoPath);
-                foreach (var item in shortcuts)
-                {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var shortcut in tmp)
-                    {
-                        string a = shortcut.Substring(shortcut.LastIndexOf('\\') + 1);
-                        string tmp2 = System.IO.Path.Combine(ShoPath, a);
-                        File.Move(shortcut, tmp2);
-                    }
-                }
-            }
-            if (printingselection == 1)
-            {
-                string PriPath = System.IO.Path.Combine(RootPath, @"3DPrinting\");
-                Directory.CreateDirectory(PriPath);
-                foreach (var item in printing)
-                {
-                    string[] tmp = Directory.GetFiles(desktopPath, item);
-                    foreach (var print in tmp)
-                    {
-                        string a = print.Substring(print.LastIndexOf('\\') + 1);
-                        string tmp2 = System.IO.Path.Combine(PriPath, a);
-                        File.Move(print, tmp2);
+                        string fileName = Path.GetFileName(file);
+                        string destination = Path.Combine(categoryPath, fileName);
+                        File.Move(file, destination);
                     }
                 }
             }
